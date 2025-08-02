3464dc6f9aa7066d964cd4d897d22b1e3dc43463
m2460901702 <m2460901702@gmail.com>
2025-08-02 18:57:24 -0400

    Update Search_AltDataStream.ps1

--- Search_AltDataStream.ps1
+++ Search_AltDataStream.ps1
@@ -1,36 +1,6 @@
 ﻿#Recursively search for alternate data streams from
 #Current directory or supplied path 
 
-function Write-Files {
-    param([System.IO.FileSystemInfo[]] $current_files)
-foreach ($f in $current_files) {
-   $n = $f.FullName
-   $streams = Get-Item $n -Stream *
-   
-   foreach ($s in $streams.Stream) {
-    if ($s -notmatch ":$DATA") {
-        echo "$n -> $s"
-    }
-   }
-}}
+function Write-Files {param([System.IO.FileSystemInfo[]] $current_files) foreach ($f in $current_files) {$n = $f.FullName ; $streams = Get-Item $n -Stream * ; foreach ($s in $streams.Stream) { if ($s -notmatch ":$DATA") { echo "$n -> $s"}}}} ; function Find-Files { param([string] $path) $current_files = ls -File $path ; $current_dirs = ls -Attributes Directory $path ; Write-Files -current_files $current_files ;   foreach ($d in $current_dirs) {Find-Files -path $d.FullName}} if ($args.Length -eq 0) { Find-Files -path .\} else { Find-Files -path $args[0]}
 
-
-function Find-Files {
-    param([string] $path)
-    $current_files = ls -File $path
-    $current_dirs = ls -Attributes Directory $path
-
-    Write-Files -current_files $current_files
-    
-    foreach ($d in $current_dirs) {
-        Find-Files -path $d.FullName
-    }
-}
-
-if ($args.Length -eq 0) {
-    Find-Files -path .\
-}
-else {
-    Find-Files -path $args[0]
-}
 
